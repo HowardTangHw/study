f8ccc67215824578f18fd0c7929fa854132a1cde
HowardTangHw <2379034@qq.com>
2017-04-25 16:47:34 +0800

    learn webpack

---
+++ webpack/lesson1/src/index.js
@@ -0,0 +1,3 @@
+import{message} from "./messages";
+var app=document.getElementById('app'); 
+app.innerHTML='<p>'+message.hi+','+message.event+'6666'+"</p>";
---
+++ webpack/lesson1/src/messages.js
@@ -0,0 +1,2 @@
+var message={ hi:'您好呀', event:'88888888888888888'};
+export {message}
